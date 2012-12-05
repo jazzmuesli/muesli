0b777c85e48c0d64283b333a12677ad3649e307a
Pavel Reich <muesli@winelib.com>
2012-12-05 01:16:51 +0000

    include_once added, so you can run ./phpunit.phar XXXDateCalculatorTest.php

--- phpunit/NewDateCalculatorTest.php
+++ phpunit/NewDateCalculatorTest.php
@@ -1,4 +1,5 @@
 <?php
+include_once 'DateCalculatorTest.php';
 class NewDateCalculatorTest extends DateCalculatorTst {
 	function __construct() {
 		parent::__construct(new NewDateCalculator());
--- phpunit/OldDateCalculatorTest.php
+++ phpunit/OldDateCalculatorTest.php
@@ -1,4 +1,5 @@
 <?php
+include_once 'DateCalculatorTest.php';
 class OldDateCalculatorTest extends DateCalculatorTst {
 	function __construct() {
 		parent::__construct(new OldDateCalculator());
